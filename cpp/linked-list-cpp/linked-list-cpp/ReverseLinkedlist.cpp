5cab36a1e5804c512f03bf3410489ae710dbaa64
Bricks <Bricks@gmail.com>
2026-05-16 20:52:20 +0530

    Q2: Find Middle of Linked List

--- cpp/linked-list-cpp/linked-list-cpp/ReverseLinkedlist.cpp
+++ cpp/linked-list-cpp/linked-list-cpp/ReverseLinkedlist.cpp
@@ -40,21 +40,21 @@ void display(Node* head)
 	}
 	cout << "NULL\n";
 }
-int main()
-{
-	// Create list: 1->2->3->4->5
-
-	Node* head = new Node(1);
-	head->next = new Node(2);
-	head->next->next = new Node(3);
-	head->next->next->next = new Node(4);
-	head->next->next->next->next = new Node(5);
-
-	cout << "Original : ";
-	display(head);
-	head = ReverseLinkedlist(head);
-	cout << "Reversed :";
-	display(head);
-	return 0;
-
-}
+//int main()
+//{
+//	// Create list: 1->2->3->4->5
+//
+//	Node* head = new Node(1);
+//	head->next = new Node(2);
+//	head->next->next = new Node(3);
+//	head->next->next->next = new Node(4);
+//	head->next->next->next->next = new Node(5);
+//
+//	cout << "Original : ";
+//	display(head);
+//	head = ReverseLinkedlist(head);
+//	cout << "Reversed :";
+//	display(head);
+//	return 0;
+//
+//}
